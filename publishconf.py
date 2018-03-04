43f1ee664b3580cdecd5e28b1992666778359d5c
摩的大飚客 <gflame42@gmail.com>
2018-03-04 14:59:08 +0800

    :construction:

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://motor-taxi-master-rider.github.io'
+SITEURL = 'https://motor-taxi-master-rider.github.io/staticsite'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
